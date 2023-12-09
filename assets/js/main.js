5080247852d036a0b0cb5b0ea10e9884e5f279d1
JoshuaChris01 <joshuachris000@gmail.com>
2023-12-09 21:10:17 +0100

    New Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close'),
-navBodyClose = document.querySelector('.nav__menu')
+    navBodyClose = document.querySelector('.nav__menu')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
@@ -99,3 +99,30 @@ const displayItems = (item) => {
         item.classList.add('faq-open')
     }
 }
+/*SHOW SCROLL UP*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/**********************SCROLL UP SECTION  ACTIVE LINK ********************/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
